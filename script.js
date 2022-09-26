b5ca3db7aca0c401c857b0310b0877f3ccbb588b
mabalal <95362819+mabalal@users.noreply.github.com>
2022-09-26 19:41:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-var fruits, item;
+var website, link;
 
 
-fruits = ['mango', 'apple', 'orange', 'grapes'];
-let element_list = document.getElementById('list');
-fruits.forEach((item) => {
+website = ['Amazon', 'Ebay', 'Littlewoods', 'Debenhams'];
+link = ['https://www.amazon.co.uk/', 'https://www.ebay.co.uk/', 'https://www.littlewoods.com/', 'https://www.debenhams.com/'];
+while (!!website.length) {
+  if(--window.LoopTrap <= 0) throw "Infinite loop.";
+  let element_list = document.getElementById('list');
   let new_li = document.createElement('li');
-  new_li.innerText = item;
+  let new_a = document.createElement('a');
+  new_a.setAttribute("href", link.shift());
+  new_a.innerText = website.shift();
+
+  new_li.appendChild(new_a);
 
   element_list.appendChild(new_li);
-});
+}
